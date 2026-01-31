8fa6217c8e7b81cd02714497a9dcca1f32f47887
Lunarmist-byte <amal.s.kuamr2005@gmail.com>
2026-01-31 14:37:17 +0530

    added one more

---
+++ Competitive_P2/prique.py
@@ -0,0 +1,11 @@
+import heapq
+li=[]
+heapq.heappush(li,-6)
+heapq.heappush(li,-1)
+heapq.heappush(li,-12)
+heapq.heappush(li,-7)
+
+print(heapq.heappop(li))
+print(heapq.heappop(li))
+print(heapq.heappop(li))
+#if added - then it's max heap, if +ve its min heap
